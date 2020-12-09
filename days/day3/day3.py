1fedd0af3d5c7d4ef56c09a5280d3e046d74a4db
mcafi <matteo.carniglia@gmail.com>
2020-12-09 21:23:25 +0100

    day 3 completed

--- days/day3/day3.py
+++ days/day3/day3.py
@@ -1,9 +1,9 @@
 # DAY 3
 # Part 1: count the tree encountered moving 1 cell down and 3 cell right
-# Part 2: count the thee encountered moving down 1 tighe, 1 down 3 rigth, 1 down 5 right, 1 down 7 right, 2 down 1 right
+# Part 2: count the thee encountered moving down 1 right, 1 down 3 right, 1 down 5 right, 1 down 7 right, 2 down 1 right
 
 if __name__ == '__main__':
-    file = open("inputs.txt", "r")
+    file = open("./day3.txt", "r")
     lines = file.readlines()
     maxSize = len(lines[0]) - 1
     trees = x = 0
@@ -14,7 +14,34 @@ if __name__ == '__main__':
             x -= maxSize
         if line[x] == '#':
             trees += 1
-        found = line[x]
         x += 3
-
     print(trees)
+
+    # Part 2
+    # ty variables counting trees, where y is a = 1r1d, b = 3r1d, c = 5r1d, d = 7r1d, e = 2d1r
+    # xn variables where x is the right shift amount
+    ta = tb = tc = td = te = x1 = x3 = x5 = x7 = 0
+    for i, line in enumerate(lines):
+        if x1 >= maxSize:
+            x1 -= maxSize
+        if x3 >= maxSize:
+            x3 -= maxSize
+        if x5 >= maxSize:
+            x5 -= maxSize
+        if x7 >= maxSize:
+            x7 -= maxSize
+        if line[x1] == '#':
+            ta += 1
+            if i % 2 == 0:
+                te += 1
+        if line[x3] == "#":
+            tb += 1
+        if line[x5] == "#":
+            tc += 1
+        if line[x7] == "#":
+            td += 1
+        x1 += 1
+        x3 += 3
+        x5 += 5
+        x7 += 7
+    print(ta * tb * tc * td * te)
