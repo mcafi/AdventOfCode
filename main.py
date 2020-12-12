4465b07db883b051cab45e0a09a63f010b02fe55
mcafi <matteo.carniglia@gmail.com>
2020-12-12 23:18:40 +0100

    day 10 part 1

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import re
 import math
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     file = open("inputs.txt", "r")
@@ -12,4 +11,21 @@ if __name__ == '__main__':
     for line in lines:
         adapters.append(int(line))
     adapters.sort()
-    print(adapters)
+
+    last = adapters[len(adapters) - 1]
+
+    device = last + 3
+
+    jump1 = 0
+    jump3 = 1  # i know the last jump is +3
+    curr = 0
+
+    while curr != last:
+        if curr + 1 in adapters:
+            jump1 += 1
+            curr += 1
+        else:
+            jump3 += 1
+            curr += 3
+
+    print(jump1 * jump3)
